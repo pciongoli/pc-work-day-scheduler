861f251eb6a60d5dabfcf2968b5180bafc20b97e
pcion <patrick.ciongoli@gmail.com>
2022-02-04 01:28:50 -0500

    added div classes for timeblocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,26 @@
 $(function() {});
 
+// create a variable for each id with class time-block
+
 // link current day and time to currentDay id via moment.js
 var currentDayEl = moment().format('MMMM Do YYYY, h:mm:ss:a');
-
+// use jquery to display text
 $("#currentDay").text(currentDayEl);
 
 
 
-// create an array for each of the work day hours
 
-// create containers for each of the elements in the array in the form of rows
+// link the current time te each hour
 
-//  create a function that will change the color of the task 
+
+//  create a function that will change the color of the task Use classes that were provided in CSS
 
 // add save button for each individual time block that saves text event to local storage
 
+// add hover to each button
+
 // add event persistence
+
+
+
+// figure out how to get time to count in real time.
