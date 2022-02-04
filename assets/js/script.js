590a2d483e7a7c346c58b8fd26f998c36e85133e
pcion <patrick.ciongoli@gmail.com>
2022-02-04 00:05:11 -0500

    minor changes

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var currentDayEl = document.querySelector("#currentDay")
+
+var currentDay = moment.format('MMMM Do YYYY, h:mm:ss:a');
+
+currentDayEl(currentDay)
+
+// link current day and time to currentDay id via moment.js
+
+// create an array for each of the work day hours
+
+// create containers for each of the elements in the array in the form of rows
+
+//  create a function that will change the color of the task 
+
+// add save button for each individual time block that saves text event to local storage
+
+// add event persistence
