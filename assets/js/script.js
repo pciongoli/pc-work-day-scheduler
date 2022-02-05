4ddf6a3ad80f7588501ad148c76dfe351ddf5e97
pcion <patrick.ciongoli@gmail.com>
2022-02-05 12:28:08 -0500

    updated container elements and created js variables for each id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,32 @@
-$(function() {});
+// put javascript code in $(document).ready(function) so that everything loads in all together
+$( document ).ready(function() {
+    var currentDayEl = moment().format('MMMM Do YYYY, h:mm:ss a');
+    // use jquery to display text
+    $("#currentDay").text(currentDayEl);
+
+    // create a variable for each hour id
+    var hour09 = $('#9-am');
+    var hour10 = $('#10-am');
+    var hour11 = $('#11-am');
+    var hour12 = $('#12-pm');
+    var hour01 = $('#1-pm');
+    var hour02 = $('#2-pm');
+    var hour03 = $('#3-pm');
+    var hour04 = $('#4-pm');
+    var hour05 = $('#5-pm');
+
+
+
+    
+    
+
+})
+
 
 // create a variable for each id with class time-block
 
 // link current day and time to currentDay id via moment.js
-var currentDayEl = moment().format('MMMM Do YYYY, h:mm:ss:a');
-// use jquery to display text
-$("#currentDay").text(currentDayEl);
+
 
 
 
